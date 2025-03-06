d363b7f763e2804e61b870a6c4c5ba8e4ed0c64a
himanshuDiro <himanshu@diro.io>
2025-03-06 23:03:50 +0530

    Added support for negative numbers

--- StringCalculator.js
+++ StringCalculator.js
@@ -5,11 +5,30 @@ function add(numbers) {
       return 0;
     }
     
-    const nums = numbers.replace(/\n/g, ',').split(',');
+    let delimiter = ',';
+    let numbersString = numbers;
+    
+    if (numbers.startsWith('//')) {
+      const delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEndIndex);
+      numbersString = numbers.substring(delimiterEndIndex + 1);
+    }
+    
+    // Replace newlines with the delimiter, then split by the delimiter
+    const nums = numbersString.replace(/\n/g, delimiter).split(delimiter);
     let sum = 0;
+    const negativeNumbers = [];
     
     for (const num of nums) {
-      sum += parseInt(num);
+      const n = parseInt(num);
+      if (n < 0) {
+        negativeNumbers.push(n);
+      }
+      sum += n;
+    }
+    
+    if (negativeNumbers.length > 0) {
+      throw new Error(`negative numbers not allowed ${negativeNumbers[0]}`);
     }
     
     return sum;
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -23,4 +23,14 @@ describe('String Calculator', () => {
     expect(add('1\n2,3')).toBe(6);
     expect(add('1,2\n3')).toBe(6);
   });
+  
+  test('should support custom delimiters', () => {
+    expect(add('//;\n1;2')).toBe(3);
+    expect(add('//|\n1|2|3')).toBe(6);
+    expect(add('//sep\n1sep2sep3')).toBe(6);
+  });
+  
+  test('should throw exception for negative numbers', () => {
+    expect(() => add('1,-2')).toThrow('negative numbers not allowed -2');
+  });
 });
