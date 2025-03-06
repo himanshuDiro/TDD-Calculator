d3c04679eafc5f0b90a2e58d73a3df00f27d6a9d
himanshuDiro <himanshu@diro.io>
2025-03-06 23:05:28 +0530

    Improved code organization with helper functions and more functional programming style for better readability

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,10 +1,11 @@
 // StringCalculator.js
 
-function add(numbers) {
-    if (numbers === '') {
-      return 0;
-    }
-    
+/**
+ * Extracts delimiter and number string from the input
+ * @param {string} numbers - Input string
+ * @returns {Object} - Object containing delimiter and numbersString
+ */
+function extractDelimiterInfo(numbers) {
     let delimiter = ',';
     let numbersString = numbers;
     
@@ -14,24 +15,40 @@ function add(numbers) {
       numbersString = numbers.substring(delimiterEndIndex + 1);
     }
     
-    // Replace newlines with the delimiter, then split by the delimiter
-    const nums = numbersString.replace(/\n/g, delimiter).split(delimiter);
-    let sum = 0;
-    const negativeNumbers = [];
-    
-    for (const num of nums) {
-      const n = parseInt(num);
-      if (n < 0) {
-        negativeNumbers.push(n);
-      }
-      sum += n;
-    }
+    return { delimiter, numbersString };
+  }
+  
+  /**
+   * Validates for negative numbers and throws if found
+   * @param {Array} numbers - Array of parsed numbers
+   */
+  function validateNegativeNumbers(numbers) {
+    const negativeNumbers = numbers.filter(n => n < 0);
     
     if (negativeNumbers.length > 0) {
       throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`);
     }
+  }
+  
+  /**
+   * Adds numbers from a comma-separated string
+   * @param {string} numbers - Input string with numbers
+   * @returns {number} - Sum of numbers
+   */
+  function add(numbers) {
+    if (numbers === '') {
+      return 0;
+    }
     
-    return sum;
+    const { delimiter, numbersString } = extractDelimiterInfo(numbers);
+    
+    // Replace newlines with the delimiter, then split by the delimiter
+    const numStrings = numbersString.replace(/\n/g, delimiter).split(delimiter);
+    const nums = numStrings.map(n => parseInt(n));
+    
+    validateNegativeNumbers(nums);
+    
+    return nums.reduce((sum, num) => sum + num, 0);
   }
   
   module.exports = { add };
