52c57bca7dc66f574d4ad07ed719321d60d6bf76
himanshuDiro <himanshu@diro.io>
2025-03-06 22:59:32 +0530

    Implemented handling for two comma-separated numbers

--- StringCalculator.js
+++ StringCalculator.js
@@ -4,7 +4,13 @@ function add(numbers) {
     if (numbers === '') {
       return 0;
     }
-    return parseInt(numbers);
+    
+    const nums = numbers.split(',');
+    if (nums.length === 1) {
+      return parseInt(nums[0]);
+    }
+    
+    return parseInt(nums[0]) + parseInt(nums[1]);
   }
   
   module.exports = { add };
